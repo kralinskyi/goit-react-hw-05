55d427c2a3aee8c6cb1bc134ff9843e84def8471
Kralinskyi Ivan <i.kralinski@gmail.com>
2024-03-06 18:49:30 +0200

    70%

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -29,47 +29,66 @@ export default function MovieDetailsPage() {
 
   return (
     <>
-      <BackToHomePageLink />
-      <div className={css.center}>{isLoading && <Loader />}</div>
+      {<div className={css.center}>{isLoading && <Loader />}</div>}
 
       {error && <p>Something wrong...</p>}
 
       {movie && (
-        <div className={css.movie}>
-          <div className={css.image_container}>
-            <img
-              className={css.poster}
-              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-              alt={movie.title}
-            />
-          </div>
-          <div className={css.details}>
-            <h2 className={css.title}>{movie.title}</h2>
-            <p className={css.overview}>{movie.overview}</p>
-            <div className={css.metadata}>
-              <p className={css.release}>
-                <b>Release Date: </b>
-                {movie.release_date}
-              </p>
-              <p className={css.budget}>
-                <b>Budget: </b>$ {movie.budget}
-              </p>
-              <p className={css.rating}>
-                <b>Rating:</b> {movie.vote_average}
-              </p>
-              <p className={css.genres}>
-                <b>Genres:</b>{" "}
-                {movie.genres.map((genre) => genre.name).join(", ")}
-              </p>
-              <a
-                href={movie.homepage}
-                target="_blank"
-                rel="noopener noreferrer">
-                Movie home page
-              </a>
+        <>
+          <BackToHomePageLink />
+          <div className={css.movie}>
+            <div className={css.image_container}>
+              <img
+                className={css.poster}
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.title}
+              />
+            </div>
+            <div className={css.details}>
+              <h2 className={css.title}>
+                {movie.title} ({new Date(movie.release_date).getFullYear()})
+              </h2>
+              <p className={css.overview}>{movie.overview}</p>
+              <div className={css.metadata}>
+                <p className={css.release}>
+                  <b>Release Date: </b>
+                  {movie.release_date}
+                </p>
+                <p className={css.budget}>
+                  <b>Budget: </b>$ {movie.budget}
+                </p>
+                <p className={css.rating}>
+                  <b>Rating:</b> {movie.vote_average}
+                </p>
+                <p className={css.genres}>
+                  <b>Genres:</b>{" "}
+                  {movie.genres.map((genre) => genre.name).join(", ")}
+                </p>
+                <div className={css.production}>
+                  {movie.production_companies[0].logo_path && (
+                    <b>Production companies:</b>
+                  )}
+                  {movie.production_companies.map((company) => (
+                    <div key={company.id}>
+                      {company.logo_path && (
+                        <img
+                          src={`https://image.tmdb.org/t/p/w200${company.logo_path}`}
+                          alt={company.name}
+                        />
+                      )}
+                    </div>
+                  ))}
+                </div>
+                <a
+                  href={movie.homepage}
+                  target="_blank"
+                  rel="noopener noreferrer">
+                  Movie home page
+                </a>
+              </div>
             </div>
           </div>
-        </div>
+        </>
       )}
     </>
   );
