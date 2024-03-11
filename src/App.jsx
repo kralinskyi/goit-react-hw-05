729557a843a35cbe5ab66244b2ba2deb7133df31
Kralinskyi Ivan <i.kralinski@gmail.com>
2024-03-11 10:57:29 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ export default function App() {
             <Route path="cast" element={<MovieCast />} />
             <Route path="reviews" element={<MovieReviews />} />
           </Route>
-          <Route path="/*" element={<NotFoundPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Suspense>
     </>
